5c551f7a795f78fe90e460515d5833e93d5f8109
yazbekludmila <yazbekludmila@gmail.com>
2021-04-01 03:20:30 -0300

    first commit

--- environment.py
+++ environment.py
@@ -13,46 +13,20 @@ from webautomators.extended_options import WebChromeOptions
 ##from pages.pages.PagamentoPage import Fazer_Pagamento
 
 from pages.pages.Abrir_Site_Page  import Abrir_Site
-from pages.pages.Efetuar_Login_Page import Fazer_Login
-from pages.pages.Consulta_Conteudo_Page import Consulta_Conteudo
-
-from pages.pages.Inclui_Conteudo_Page import Inclui_Conteudo
-from pages.pages.Exclui_Conteudo_Page import Exclui_Conteudo
-from pages.pages.Altera_Conteudo_Page import Altera_Conteudo
-
-##from pages.pages.EdicaoUsuarioPage import Selecionar_Informacao_Alterar
-##from pages.pages.EdicaoEnderecoPage import Alterar_Endereco
-##from pages.pages.CadastroUsuarioPage import Cadastrar_Usuario
+from pages.pages.Consultar_Programacao_Atual_Page import Consultar_Validar_Programacao_Atual
 
 def before_all(context):
 	options = WebChromeOptions()
 	options.maximized()
-	#context.app = webdriver.Chrome(executable_path='./driver/chromedriver.exe')
-	#context.app = WebChromeDriver(executable_path='../driver/chromedriver.exe', options=options)
-	#context.app = WebChromeDriver(executable_path='../driver/chromedriver.exe', options=options)
 	context.app = WebChromeDriver(executable_path='C:\drivers\chromedriver.exe', options=options)
 	#context.app = WebChromeDriver(executable_path='C:\Users\Ludmila\Documents\A4_YOU_SEE\DESAFIO_4_YOU_SEE\saraivatestes\driver\chromedriver.exe', options=options)
-	##HOJE context.home  = Procura_Compra_Produto(context.app)
-	##HOJE login = Fazer_Login_Saraiva(context.app)
-	##HOJE context.frete = Escolher_Frete(context.app)
-	##HOJE context.pagamento = Fazer_Pagamento(context.app) 
-
 	
+	context.ConsultaValidaConteudo = Consultar_Validar_Programacao_Atual(context.app)
+
 	context.abresite  = Abrir_Site(context.app)
-	context.login = Fazer_Login(context.app)
-	context.consultaconteudo  = Consulta_Conteudo(context.app)
 	
-	context.excluiconteudo  = Exclui_Conteudo(context.app)
-	context.incluiconteudo  = Inclui_Conteudo(context.app)
-	context.alteraconteudo  = Altera_Conteudo(context.app)
-
-	# HOJE context.edicaousuario = Selecionar_Informacao_Alterar(context.app)
-	# HOJE context.edicaoendereco = Alterar_Endereco(context.app) 
-	
-	##HOJE #context.cadastrousuario = Cadastrar_Usuario(context.app) 
-
 	context.pages = {
-	    "login": "https://qa3.4yousee.com.br/"
+	#    "login": "http://www.sky.com.br"
 	#	"login": "https://www.saraiva.com.br/checkout/onepage/"
 	#	"login": "https://www.saraiva.com.br/checkout/onepage/",
 	#	"frete": "https://www.saraiva.com.br/checkout/onepage/",
